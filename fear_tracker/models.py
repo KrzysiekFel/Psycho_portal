3c9e2f5a66dbb485ab5133bab88e7de821a48ec5
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-07-01 18:22:44 +0200

    feat(fear_tracker): add model

--- fear_tracker/models.py
+++ fear_tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+ANXIETY_CHOICES = [
+    (1, 'Minimal anxiety'),
+    (2, 'Mild anxiety'),
+    (3, 'Moderate anxiety'),
+    (4, 'Intense anxiety'),
+    (5, 'Severe anxiety'),
+]
+
+
+class FearTracker(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    activity = models.CharField(max_length=100)
+    fear_level = models.IntegerField(choices=ANXIETY_CHOICES)
+    disturbing_thoughts = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Fear Tracker - {self.date} {self.time} - Activity: {self.activity}"
