e6c3d0a9d2a4bf3a99b555a708ba5c4fd528136a
Krzysiek <krzysztof.feliksiak@gmail.com>
2023-07-02 10:50:09 +0200

    chore(git): update .gitignore

--- fear_tracker/urls.py
+++ fear_tracker/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import fear_tracker, plot_fear, FearCreateView
+from .views import fear_tracker, YourChartView, FearCreateView
 
 urlpatterns = [
     path('', fear_tracker, name='fear-tracker'),
     path('new/', FearCreateView.as_view(), name='fear-create'),
-    path('plot/', plot_fear, name='fear-plot')
+    path('plot/', YourChartView.as_view(), name='fear-plot')
 ]
--- fear_tracker/views.py
+++ fear_tracker/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import FearTracker
-from.forms import FearTrackerForm
+from .forms import FearTrackerForm
 from django.contrib import messages
+import matplotlib.pyplot as plt
+from django.conf import settings
+import os
 
 
 def fear_tracker(request):
     return render(request, 'fear_tracker.html', {'title': 'Fear Tracker'})
-
-
-def plot_fear(request):
-    return render(request, 'plot_fear.html', {'title': 'Plot fear'})
 
 
 class FearCreateView(LoginRequiredMixin, CreateView):
@@ -25,3 +24,27 @@ class FearCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         messages.success(self.request, 'Record added successfully.')
         return super().form_valid(form)
+
+
+class YourChartView(TemplateView):
+    template_name = 'plot_fear.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = FearTracker.objects.all()
+
+        x = [f.date.strftime('%Y-%m-%d %H:%M:%S') for f in queryset]
+        y = [f.fear_level for f in queryset]
+
+        plt.plot(x, y)
+        plt.xlabel('Date and time')
+        plt.ylabel('Fear level')
+        plt.title('Fear level plot')
+
+        chart_image = 'media/chart_image.jpg'
+        plt.savefig(chart_image)
+        plt.close()
+
+        context['chart_image'] = os.path.join(settings.MEDIA_URL, chart_image)
+
+        return context
